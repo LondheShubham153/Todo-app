fac172f96fc04c5859f039010fc082c98e66a572
Shubham Londhe <shubhaml@heaptrace.com>
2020-01-03 19:46:30 +0530

    initial commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include                 # add this
+from rest_framework import routers                    # add this
+from todo import views       
+from django.conf.urls import url                     # add this
+        
+router = routers.DefaultRouter()                      # add this
+router.register(r'todos', views.TodoView, 'todo')     # add this
+        
+urlpatterns = [
+    path('admin/', admin.site.urls),           
+    path('api/', include(router.urls))                # add this
+]
